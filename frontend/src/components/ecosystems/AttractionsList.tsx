c894d92ab9c82b450bbb83ea9ce9fef46638abe7
olawwska <olwojciech@gmail.com>
2023-06-06 20:02:59 +0200

    add basic map

--- frontend/src/components/ecosystems/AttractionsList.tsx
+++ frontend/src/components/ecosystems/AttractionsList.tsx
@@ -1,7 +1,8 @@
-import { FC, useState } from 'react';
+import { FC, useState, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
+import { useLoadScript, GoogleMap } from '@react-google-maps/api';
 // components
-import { Box, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 // subcomponents
 import TextFieldPlace from '../atoms/TextFieldPlace';
 import ButtonSubmitPlace from '../atoms/ButtonSubmitPlace';
@@ -11,8 +12,22 @@ import NoPlacesPage from '../molecules/NoPlacesPage';
 // API
 import useAttractions from '../useAttractions';
 import useCities from '../useCities';
+// styles
+import useStyles from './useStyles';
 
 const AttractionsList: FC = () => {
+  const classes = useStyles();
+  const { isLoaded } = useLoadScript({
+    // @ts-ignore
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+  });
+  const center = useMemo(
+    () => ({
+      lat: 40.73061,
+      lng: -73.935242,
+    }),
+    []
+  );
   const [inputVal, setInputVal] = useState('');
   const { cityId } = useParams();
 
@@ -34,38 +49,41 @@ const AttractionsList: FC = () => {
   };
 
   return (
-    <Box
-      sx={{
-        width: '50%',
-        margin: '0 auto',
-        marginTop: '15%',
-      }}
-    >
-      {Boolean(attractions?.length) ? (
-        <>
-          <ListTitle title={`${titleText} attractions list`} />
-          <ListPlaces places={attractions} onHandleDelete={handleDeleteAttraction} />
-          <Grid item xs={12}>
-            <TextFieldPlace
-              label={'Type an attraction'}
-              setInputVal={setInputVal}
-              inputVal={inputVal}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <ButtonSubmitPlace handleSubmit={handleAddAttraction} text={'Add an attraction'} />
-          </Grid>
-        </>
-      ) : (
-        <NoPlacesPage
-          inputVal={inputVal}
-          setInputVal={setInputVal}
-          label={'Add attraction'}
-          title={'There are no attractions added to this city yet'}
-          onHandleSubmit={handleAddAttraction}
-        />
-      )}
-    </Box>
+    <Grid container>
+      <Grid item xs={6} style={{ height: '100vh', marginTop: '10%', padding: '0 5%' }}>
+        {Boolean(attractions?.length) ? (
+          <>
+            <ListTitle title={`${titleText} attractions list`} />
+            <ListPlaces places={attractions} onHandleDelete={handleDeleteAttraction} />
+            <Grid item xs={12}>
+              <TextFieldPlace
+                label={'Type an attraction'}
+                setInputVal={setInputVal}
+                inputVal={inputVal}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <ButtonSubmitPlace handleSubmit={handleAddAttraction} text={'Add an attraction'} />
+            </Grid>
+          </>
+        ) : (
+          <NoPlacesPage
+            inputVal={inputVal}
+            setInputVal={setInputVal}
+            label={'Add attraction'}
+            title={'There are no attractions added to this city yet'}
+            onHandleSubmit={handleAddAttraction}
+          />
+        )}
+      </Grid>
+      <Grid item xs={6} style={{ height: '100vh' }}>
+        {!isLoaded ? (
+          <>isLoading</>
+        ) : (
+          <GoogleMap mapContainerClassName={classes.map} center={center} zoom={10}></GoogleMap>
+        )}
+      </Grid>
+    </Grid>
   );
 };
 
---
+++ frontend/src/components/ecosystems/useStyles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+}));
+
+export default useStyles;
