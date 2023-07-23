fb7fa36788a0a629c58f6fb92e7d2d448ee20979
Aleksandra Wojciechowska <olwojciech@gmail.com>
2023-07-23 16:51:05 +0200

    fix api key in template string

--- frontend/src/components/useCoordinates.ts
+++ frontend/src/components/useCoordinates.ts
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query';
 
 const useCoordinates = () => {
   const getCityCoordinates = async (city: string) => {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=process.env.REACT_APP_API_KEY`;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.REACT_APP_API_KEY}`;
     const { data } = await axios.get(url);
     return data;
   };
